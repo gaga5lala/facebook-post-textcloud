1e081cb00d06a2f1a7dfed10b81ff91c4a977bbb
gaga5lala <gaga5lala1013@gmail.com>
2017-11-06 10:50:38 +0800

    commit all

---
+++ run-docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -it --name facebook-post-textcloud -v /Users/gaga/github/gaga5lala/facebook-post-textcloud/code:/usr/local/lib/python2.7/site-packages/jieba/code facebook-post-textcloud
